3d8138bd3590348e7b494d4c5198565c4720eff2
thewhitelisted <clee2k6@outlook.com>
2025-05-27 15:49:05 -0400

    impl: hitignore support for commits

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -12,12 +12,40 @@ pub fn add(path: &str) {
         std::process::exit(1);
     }
 
+    // create ignore list
+    if path_buf.starts_with(".hit") {
+        eprintln!("fatal: cannot add files in .hit directory");
+        std::process::exit(1);
+    }
+    // go through .hitignore and create a list of ignored files/directories
+    let hitignore = fs::read_to_string(".hitignore").unwrap_or_default();
+    let ignored_paths: Vec<String> = hitignore
+        .clone()
+        .lines()
+        .filter_map(|line| {
+            let trimmed = line.trim();
+            if !trimmed.is_empty() && !trimmed.starts_with('#') {
+                Some(trimmed.to_string())
+            } else {
+                None
+            }
+        })
+        .collect();
+    // check if the path is in the ignore list
+    if ignored_paths
+        .iter()
+        .any(|ignore| path_buf.starts_with(ignore))
+    {
+        eprintln!("fatal: '{}' is ignored by .hitignore", path);
+        std::process::exit(1);
+    }
+
     let mut index = Index::load();
 
     if path_buf.is_file() {
         add_file(&path_buf, &mut index);
     } else if path_buf.is_dir() {
-        add_directory(&path_buf, &mut index);
+        add_directory(&path_buf, &mut index, hitignore);
     } else {
         eprintln!("fatal: '{}' is not a valid file or directory", path);
         std::process::exit(1);
@@ -40,7 +68,29 @@ fn add_file(path: &Path, index: &mut Index) {
     index.add(entry);
 }
 
-fn add_directory(dir: &Path, index: &mut Index) {
+fn add_directory(dir: &Path, index: &mut Index, ignorelist: String) {
+    // Normalize the directory path
+    let rel_dir = dir.strip_prefix(".").unwrap_or(dir);
+    let rel_str = "/".to_owned() + &rel_dir.to_str().unwrap().replace("\\", "/"); // normalize for Windows
+    let ignore_paths: Vec<&str> = ignorelist
+        .lines()
+        .filter_map(|line| {
+            let trimmed = line.trim();
+            if !trimmed.is_empty() && !trimmed.starts_with('#') {
+                Some(trimmed)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    // check if the directory is in the ignore list
+    if ignore_paths
+        .iter()
+        .any(|ignore| rel_str.starts_with(ignore))
+    {
+        return;
+    }
     for entry in fs::read_dir(dir).expect("Failed to read directory") {
         let entry = entry.expect("Failed to read entry");
         let path = entry.path();
@@ -52,7 +102,7 @@ fn add_directory(dir: &Path, index: &mut Index) {
         if path.is_file() {
             add_file(&path, index);
         } else if path.is_dir() {
-            add_directory(&path, index);
+            add_directory(&path, index, ignorelist.clone());
         }
     }
 }
